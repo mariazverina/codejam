28bda875a45ae168c3671b5c1d0b200c982aa1a7
Maria Zverina <maria@zverina.co.uk>
2012-05-05 16:37:24 +0100

    meow

--- runSmall.sh
+++ runSmall.sh
@@ -1,5 +1,5 @@
 source probid.sh
 echo Solving problem $PROBID try #$1
-cat $PROBID*small*$1.in | python src/main.py > $PROBID.small.out
+cat $PROBID*small*$1.in | python src/main.py multi script > $PROBID.small.out
 cat $PROBID.small.out
 sh mkzip.sh
